982e20703a6af78ae8db275922da7beba9d63ee3
Irina <strelaweb@gmail.com>
2020-05-05 16:17:45 +1000

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function(){
 
+	$('.slider').slick({
+	  autoplay: true,
+	  arrows:true,
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  centerMode: true,
+	  centerPadding: '60px'
+  	  //variableWidth: true
+	});
 
 });
