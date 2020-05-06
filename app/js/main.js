ae2e681392a45439fb32c0e41d824aa488e3f8be
Irina <strelaweb@gmail.com>
2020-05-06 11:49:52 +1000

    added slider about

--- app/js/main.js
+++ app/js/main.js
@@ -30,4 +30,20 @@ $(function(){
         $('.header__menu-burger').children().css('background-color','#fff');
         
     });
+
+    $('.about__slider').slick({
+	  autoplay: true,
+	  arrows:true,	  
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  speed: 800,
+	  fade: true	  
+	});
+
+	//let currentSlide = $('.about__slider')).slick('slickCurrentSlide') + 1;
+	$(".about__slider").on("beforeChange", function(event, slick, currentSlide, nextSlide){
+		//let currentSlide = currentSlide + 1;
+    	$(".current_slide").text('0' + (currentSlide + 1));
+	});
+
 });
